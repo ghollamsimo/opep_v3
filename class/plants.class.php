ad6c43f18488c0b7e293c8124c3e328defb6c05d
ghollamsimo <ghollamsimo1@gmail.com>
2023-12-19 16:35:44 +0100

    Now You Can add Plant And Delete Them

---
+++ class/plants.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Admin{
+    private $db;
+
+    function __construct()
+    {
+        $this->db = new DatabaseConnection();
+    }
+
+    public function addplants(){
+        $insert = "INSERT INTO ()"
+    }
+}
